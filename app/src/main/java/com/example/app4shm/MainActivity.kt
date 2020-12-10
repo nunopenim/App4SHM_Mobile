1b3370f34c6297aabd49387d5d647fdfdb3ef27a
nunopenim <nuno.penim@gmail.com>
2020-12-10 14:38:08 +0000

    general: Just a single screen now

--- app/src/main/java/com/example/app4shm/MainActivity.kt
+++ app/src/main/java/com/example/app4shm/MainActivity.kt
@@ -1,22 +1,50 @@
 package com.example.app4shm
 
-import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.content.Context
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+var isReading = false
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var mSensorManager: SensorManager
+    private lateinit var mAccelerometer: Sensor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.content_main)
-
-
-        var button = findViewById(R.id.goToReadingActivity) as Button
-
-        button.setText("Start Measuring")
-
+        var button = findViewById(R.id.startMeasuring) as Button
         button.setOnClickListener {
-            startActivity(Intent(this, ReadingActivity::class.java))
+            isReading = !isReading
+            if (isReading) {
+                button.setText("Stop Measuring")
+            }
+            else {
+                button.setText("Start Measuring")
+            }
+        }
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val x = findViewById<TextView>(R.id.x)
+        val y = findViewById<TextView>(R.id.y)
+        val z = findViewById<TextView>(R.id.z)
+        if (isReading) {
+            x.setText(event.values[0].toString())
+            y.setText(event.values[1].toString())
+            z.setText(event.values[2].toString())
         }
     }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
 }
+
