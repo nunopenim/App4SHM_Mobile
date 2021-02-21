345f837e46b40add686b0882a3d0c3061a6dbd92
Paulo <paulo@paulo>
2021-02-21 15:45:44 +0000

    Gráfico

--- app/src/main/java/org/app4shm/demo/MainActivity.kt
+++ app/src/main/java/org/app4shm/demo/MainActivity.kt
@@ -7,9 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Context
+import android.graphics.Color
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.widget.TextView
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import java.lang.Math.sqrt
 
 // Variaveis Globais
 var isReading = false
@@ -17,9 +22,17 @@ var readings = arrayListOf<String>()
 var time = 0.0
 var startTime = System.currentTimeMillis()
 
+var series1 = LineGraphSeries<DataPoint>();
+var series2 = LineGraphSeries<DataPoint>();
+var series3 = LineGraphSeries<DataPoint>();
+
+
+
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var mSensorManager: SensorManager
     private lateinit var mAccelerometer: Sensor
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.content_main)
@@ -41,11 +54,18 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent) {
+        var graph: GraphView = findViewById(R.id.graph)
         if (isReading) {
-            val values = findViewById<TextView>(R.id.values)
+
+
+            //val values = findViewById<TextView>(R.id.values)
             val x = String.format("%.2f", event.values[0])
             val y = String.format("%.2f", event.values[1])
             val z = String.format("%.2f", event.values[2])
+
+            val accsqr = x.toDouble() * x.toDouble() + y.toDouble() * y.toDouble() + z.toDouble() * z.toDouble()
+            val sclracc = sqrt(accsqr)
+
             val timestr = String.format("%.2f", time)
             val readout = "time=$timestr s | x=$x (m/s^2) | y=$y (m/s^2) | z=$z (m/s^2)\n"
             if (readings.size < 25) {
@@ -56,8 +76,20 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 readings.add(readout)
             }
             val lstPrint = listStringificator(readings)
-            values.setText(lstPrint)
+            //values.setText(lstPrint)
             time = (((System.currentTimeMillis() - startTime).toDouble())/1000)
+
+            series1.appendData(DataPoint(time,x.toDouble()), true, 100)
+            series2.appendData(DataPoint(time,y.toDouble()), true, 100)
+            series3.appendData(DataPoint(time,z.toDouble()), true, 100)
+            series2.setColor(Color.RED)
+            series3.setColor(Color.GREEN)
+
+            graph.addSeries(series1)
+            graph.addSeries(series2)
+            graph.addSeries(series3)
+
+
             Thread.sleep(20) //50 hz
         }
     }
