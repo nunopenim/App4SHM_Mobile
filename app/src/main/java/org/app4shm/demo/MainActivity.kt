7987f7b838d5af965601871c8df07a2b30acb035
nunopenim <nuno.penim@gmail.com>
2021-02-26 14:45:32 +0000

    general: misc fixes to app, code cleaning - Button to stop measuring now cleans the graph series too (but doesn't delete the graph) - series parameters are now set when the instance is created, and not when the sensor state changes (performance improvement) - miscellaneous changes to prepare the app to run on threads

--- app/src/main/java/org/app4shm/demo/MainActivity.kt
+++ app/src/main/java/org/app4shm/demo/MainActivity.kt
@@ -25,12 +25,9 @@ var series1 = LineGraphSeries<DataPoint>();
 var series2 = LineGraphSeries<DataPoint>();
 var series3 = LineGraphSeries<DataPoint>();
 
-
-
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var mSensorManager: SensorManager
     private lateinit var mAccelerometer: Sensor
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +40,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 startTime = System.currentTimeMillis()
             }
             else {
+                series1 = LineGraphSeries<DataPoint>();
+                series2 = LineGraphSeries<DataPoint>();
+                series3 = LineGraphSeries<DataPoint>();
                 time = 0.0
                 button.setText("Start Measuring")
             }
@@ -50,6 +50,12 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+
+        series1.setThickness(8)
+        series2.setThickness(8)
+        series3.setThickness(8)
+        series2.setColor(Color.RED)
+        series3.setColor(Color.GREEN)
     }
 
     override fun onSensorChanged(event: SensorEvent) {
@@ -59,33 +65,26 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             val x = String.format("%.2f", event.values[0])
             val y = String.format("%.2f", event.values[1])
             val z = String.format("%.2f", event.values[2])
-            val timestr = String.format("%.2f", time)
-            val readout = "time=$timestr s | x=$x (m/s^2) | y=$y (m/s^2) | z=$z (m/s^2)\n"
-            if (readings.size < 25) {
-                readings.add(readout)
-            }
-            else {
-                readings.removeAt(0)
-                readings.add(readout)
-            }
+
+            //val timestr = String.format("%.2f", time)
+            //val readout = "time=$timestr s | x=$x (m/s^2) | y=$y (m/s^2) | z=$z (m/s^2)\n"
+            //if (readings.size < 25) {
+            //    readings.add(readout)
+            //}
+            //else {
+            //    readings.removeAt(0)
+            //    readings.add(readout)
+            //}
             //val lstPrint = listStringificator(readings)
             //values.setText(lstPrint)
-            time = (((System.currentTimeMillis() - startTime).toDouble())/1000)
 
+            time = (((System.currentTimeMillis() - startTime).toDouble())/1000)
             series1.appendData(DataPoint(time,x.toDouble()), true, 100)
             series2.appendData(DataPoint(time,y.toDouble()), true, 100)
             series3.appendData(DataPoint(time,z.toDouble()), true, 100)
-
-            series1.setThickness(8) //Thicc series
-            series2.setThickness(8)
-            series3.setThickness(8)
-            series2.setColor(Color.RED)
-            series3.setColor(Color.GREEN)
-
             graph.addSeries(series1)
             graph.addSeries(series2)
             graph.addSeries(series3)
-
             Thread.sleep(20) //50 hz
         }
     }
