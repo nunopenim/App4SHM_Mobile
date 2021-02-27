8d2f3ac8a57a58f56b60327d50da7b5ffc00b876
nunopenim <nuno.penim@gmail.com>
2021-02-27 14:36:26 +0000

    general: Misc changes. No longer a clogged toilet on low end phones - Threading has been optimized, and the ThreadPool is semi-dynamic (they are fixed, but depending on each CPU, it is calculated on the program start). - The frequency is no longer adjusted inside the thread, but by the thread creator function. - Added a few comments for easier code analysis and explanation of certain design and code decisions (Mostly in Portuguese though!) - Fixed random deadlocks by making sure the semaphore is opened after the thread is done with it. - Changes to the button text, because of coherence.

--- app/src/main/java/org/app4shm/demo/MainActivity.kt
+++ app/src/main/java/org/app4shm/demo/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.app4shm.demo
 
+import android.annotation.SuppressLint
 import android.hardware.Sensor
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
@@ -15,13 +16,14 @@ import com.jjoe64.graphview.series.LineGraphSeries
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Semaphore
+import kotlin.properties.Delegates
 
 //eu sei que com variáveis globais, o código fica meio sujo, mas ainda não repensei de uma
 //maneira melhor de fazer, temos de pensar nisso depois, para já não há bugs, mas de futuro...
 
 //leituras
 var isReading = false
-var readings = arrayListOf<String>()
+//var readings = arrayListOf<String>()
 
 //medições
 var time = 0.0
@@ -31,29 +33,75 @@ var startTime = System.currentTimeMillis()
 var series1 = LineGraphSeries<DataPoint>()
 var series2 = LineGraphSeries<DataPoint>()
 var series3 = LineGraphSeries<DataPoint>()
-lateinit var graph: GraphView;
+lateinit var graph: GraphView
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     // Sensor stuff
     private lateinit var mSensorManager: SensorManager
     private lateinit var mAccelerometer: Sensor
 
-    private val executor: ExecutorService = Executors.newFixedThreadPool(4)
-    private var semaphore : Semaphore = Semaphore(1)
+    /* Ok Paulo, acho importante descrever as 4 linhas de código que estão à frente e o bloco de
+     * código inicial no onCreate(), para evitar questões.
+     *
+     * Inicialmente eu vou buscar o número de cores disponíveis. Não todos os do dispositivo, apenas os
+     * disponíveis.
+     *
+     * Posteriormente crio uma pool de threads, que com um número calculado na onCreate(). Mesmo num dispositivo dual core, é preferivel
+     * ter uma thread para um core do que ter um número fixo de threads, por exemplo 2 threads.
+     *
+     * Porquê? Simples, se tiveres 2 threads, num CPU quadcore, podes ter até 2 cores sem fazer nada, tendo falta de desempenho.
+     * Terás o desempenho melhor com um dual core. Mas num single core, o sheduler do CPU estará
+     * constantemente a alterar entre as threads, e a app poderá até ficar muito mais lenta do que com apenas
+     * 1 thread.
+     *
+     * O semáforo pronto... é um semáforo. Tem apenas uma autorização para se comportar como um semáforo binário.
+     *
+     * E sim, tenho saudades de coisas de mais baixo nível. I <3 C & Assembly. Ainda acho que era mais fácil com
+     * a biblioteca das pthreads e dos semaphores.
+     */
 
+    private val NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors()
+    private var thread_count by Delegates.notNull<Int>()
+    private lateinit var executor: ExecutorService
+    private var semaphore : Semaphore = Semaphore(1)
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Este bloco de código gera o número de threads dinamicamente
+        if (NUMBER_OF_CORES > 4) {
+            thread_count = 4
+        }
+        else if (NUMBER_OF_CORES > 1){
+            thread_count = NUMBER_OF_CORES - 1
+        }
+        else {
+            thread_count = 1
+        }
+        executor = Executors.newFixedThreadPool(thread_count)
+
         setContentView(R.layout.content_main)
         val button = findViewById<Button>(R.id.startMeasuring)
         graph = findViewById(R.id.graph)
         button.setOnClickListener {
             isReading = !isReading
             if (isReading) {
-                button.setText("Stop Measuring")
+                button.setText("Stop Reading")
                 startTime = System.currentTimeMillis()
             }
             else {
-                button.setText("Start Measuring")
+                button.setText("Start Reading")
+                graph.removeAllSeries()
+                series1 = LineGraphSeries<DataPoint>()
+                series2 = LineGraphSeries<DataPoint>()
+                series3 = LineGraphSeries<DataPoint>()
+                series1.setThickness(8)
+                series2.setThickness(8)
+                series3.setThickness(8)
+                series2.setColor(Color.RED)
+                series3.setColor(Color.GREEN)
+                graph.addSeries(series1)
+                graph.addSeries(series2)
+                graph.addSeries(series3)
             }
         }
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
@@ -74,7 +122,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         //val values = findViewById<TextView>(R.id.values)
         if (isReading) {
             time = (((System.currentTimeMillis() - startTime).toDouble())/1000)
-
             //val timestr = String.format("%.2f", time)
             //val readout = "time=$timestr s | x=$x (m/s^2) | y=$y (m/s^2) | z=$z (m/s^2)\n"
             //if (readings.size < 25) {
@@ -86,15 +133,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             //}
             //val lstPrint = listStringificator(readings)
             //values.setText(lstPrint)
-
             executor.execute {
-                semaphore.acquire()
-                series1.appendData(DataPoint(time, event.values[0].toDouble()), false, 50)
-                series2.appendData(DataPoint(time, event.values[1].toDouble()), false, 50)
-                series3.appendData(DataPoint(time, event.values[2].toDouble()), false, 50)
+                semaphore.acquire() //I miss lpthreads.h and semaphores.h
+                series1.appendData(DataPoint(time, event.values[0].toDouble()), false, 100)
+                series2.appendData(DataPoint(time, event.values[1].toDouble()), false, 100)
+                series3.appendData(DataPoint(time, event.values[2].toDouble()), false, 100)
                 semaphore.release()
-                Thread.sleep(20) //50 hz
             }
+            Thread.sleep(20) //50 hz
         }
     }
 
