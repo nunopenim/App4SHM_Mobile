10c5d032d1ee032f0f758c521f5a14abcbc8bf00
nunopenim <nuno.penim@gmail.com>
2021-03-16 15:17:12 +0000

    MainActivity: trashed realtime from gps for now

--- app/src/main/java/org/app4shm/demo/MainActivity.kt
+++ app/src/main/java/org/app4shm/demo/MainActivity.kt
@@ -1,7 +1,9 @@
 package org.app4shm.demo
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -11,7 +13,9 @@ import android.os.Bundle
 import android.provider.Settings.Secure
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.app4shm.server.Data
+import com.google.android.gms.location.*
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
@@ -19,7 +23,6 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Semaphore
 import kotlin.properties.Delegates
-
 
 //eu sei que com variáveis globais, o código fica meio sujo, mas ainda não repensei de uma
 //maneira melhor de fazer, temos de pensar nisso depois, para já não há bugs, mas de futuro...
@@ -32,7 +35,10 @@ var readings = arrayListOf<Data>()
 var time = 0.0
 
 //GPS and ID stuff
-var startTime = System.currentTimeMillis()
+//var requestingLocationUpdates = true
+//lateinit var location : Location
+var offset : Long= 0
+var startTime = System.currentTimeMillis() + offset
 var id = ""
 
 //gráficos
@@ -45,36 +51,32 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     // Sensor stuff
     private lateinit var mSensorManager: SensorManager
     private lateinit var mAccelerometer: Sensor
-    /* Ok Paulo, acho importante descrever as 4 linhas de código que estão à frente e o bloco de
-     * código inicial no onCreate(), para evitar questões.
-     *
-     * Inicialmente eu vou buscar o número de cores disponíveis. Não todos os do dispositivo, apenas os
-     * disponíveis.
-     *
-     * Posteriormente crio uma pool de threads, que com um número calculado na onCreate(). Mesmo num dispositivo dual core, é preferivel
-     * ter uma thread para um core do que ter um número fixo de threads, por exemplo 2 threads.
-     *
-     * Porquê? Simples, se tiveres 2 threads, num CPU quadcore, podes ter até 2 cores sem fazer nada, tendo falta de desempenho.
-     * Terás o desempenho melhor com um dual core. Mas num single core, o sheduler do CPU estará
-     * constantemente a alterar entre as threads, e a app poderá até ficar muito mais lenta do que com apenas
-     * 1 thread.
-     *
-     * O semáforo pronto... é um semáforo. Tem apenas uma autorização para se comportar como um semáforo binário.
-     *
-     * E sim, tenho saudades de coisas de mais baixo nível. I <3 C & Assembly. Ainda acho que era mais fácil com
-     * a biblioteca das pthreads e dos semaphores.
-     */
-
     private val NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors()
     private var thread_count by Delegates.notNull<Int>()
     private lateinit var executor: ExecutorService
     private var semaphore : Semaphore = Semaphore(1)
+    //private lateinit var locationCallback: LocationCallback
+    //lateinit var fusedLocationClient : FusedLocationProviderClient
+    //var locationRequest = LocationRequest.create()
     //private var semaphoreSend : Semaphore = Semaphore(1)
 
-    @SuppressLint("MissingPermission")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    //@SuppressLint("MissingPermission")
+    //private fun startLocationUpdates() {
+    //    fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+    //}
+
+    //override fun onResume() {
+    //    super.onResume()
+    //    startLocationUpdates()
+    //}
+
+    fun init() {
+
         // Este bloco de código gera o número de threads dinamicamente
+        //location = locateMe()
         if (NUMBER_OF_CORES > 4) {
             thread_count = 4
         }
@@ -87,9 +89,19 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         executor = Executors.newFixedThreadPool(thread_count)
 
         // id, gps stuff, you name it!
+
         id = Secure.getString(contentResolver, Secure.ANDROID_ID)
+        //locationCallback = object : LocationCallback() {
+        //    override fun onLocationResult(locationResult: LocationResult?) {
+        //        locationResult ?: return
+        //        location = locationResult.lastLocation
+        //    }
+        //}
+        //locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY;
+        //fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         //app itself
+
         setContentView(R.layout.content_main)
         val button = findViewById<Button>(R.id.startMeasuring)
         graph = findViewById(R.id.graph)
@@ -127,11 +139,39 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         graph.addSeries(series1)
         graph.addSeries(series2)
         graph.addSeries(series3)
+
+    }
+
+    //fun offsetUpdater () {
+    //    if (offset > 100) {
+    //        startLocationUpdates()
+    //    }
+    //    offset = location.time - System.currentTimeMillis()
+    //    locationCallback = object : LocationCallback() {
+    //        override fun onLocationResult(locationResult: LocationResult?) {
+    //            locationResult ?: return
+    //            location = locationResult.lastLocation
+    //        }
+    //    }
+    //}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 1111)
+
+            // Still needs work permission-wise!
+        }
+        else {
+            init()
+        }
     }
 
     override fun onSensorChanged(event: SensorEvent) {
         //val values = findViewById<TextView>(R.id.values)
         if (isReading) {
+            //offsetUpdater()
             val actualTime = System.currentTimeMillis()
             time = (((actualTime - startTime).toDouble())/1000)
             graph.getViewport().setMinX(time-5)
@@ -192,7 +232,30 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-    }
+    //@SuppressLint("MissingPermission")
+    //fun locateMe() : Location {
+    //    var gps_enabled = false
+    //    var network_enabled = false
+    //    val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    //    gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    //    network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    //    var net_loc: Location? = null
+    //    var gps_loc: Location? = null
+    //    var finalLoc: Location? = null
+    //    if (gps_enabled) gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+    //    if (network_enabled) net_loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+    //    if (gps_loc != null && net_loc != null) {
+    //        //smaller the number more accurate result will
+    //        finalLoc = if (gps_loc.accuracy > net_loc.accuracy) net_loc else gps_loc
 
+            // I used this just to get an idea (if both avail, its upto you which you want to take as I've taken location with more accuracy)
+    //    } else {
+    //        if (gps_loc != null) {
+    //            finalLoc = gps_loc
+    //        } else if (net_loc != null) {
+    //            finalLoc = net_loc
+    //        }
+    //    }
+    //    return finalLoc!!
+    //}
 }
