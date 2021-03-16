e9a0c68c078eaa5b5c0a6ef1c957752cb9a31824
nunopenim <nuno.penim@gmail.com>
2021-03-16 16:16:01 +0000

    Data: made a JSON builder, because I like to do stuff myself

--- app/src/main/java/org/app4shm/demo/Data.kt
+++ app/src/main/java/org/app4shm/demo/Data.kt
@@ -4,4 +4,8 @@ class Data(val id: String, val timeStamp: Long, val x: Float, val y: Float, val 
     override fun toString(): String {
         return "ID: ${id} | Timestamp: ${timeStamp} | X: ${x} | Y: ${y} | Z: ${z}\n"
     }
+
+    fun JSONer(): String {
+        return "{\"id\": \"${id}\", \"timeStamp\": ${timeStamp}, \"x\": ${x}, \"y\": ${y}, \"z\": ${z}}"
+    }
 }
