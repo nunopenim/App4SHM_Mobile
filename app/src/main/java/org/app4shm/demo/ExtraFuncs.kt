de08f5385ae2045707ddb97235dd81b440efc22c
nunopenim <nuno.penim@gmail.com>
2021-03-16 16:16:26 +0000

    ExtraFuncs: made a Data List to JSON String builder

--- app/src/main/java/org/app4shm/demo/ExtraFuncs.kt
+++ app/src/main/java/org/app4shm/demo/ExtraFuncs.kt
@@ -1,9 +1,21 @@
 package org.app4shm.demo
 
+import com.app4shm.server.Data
+
 fun listStringificator(list: ArrayList<String>): String {
     var str = ""
     for (i in list) {
         str += i
     }
+    return str
+}
+
+fun makeMeAJson(list: ArrayList<Data>) : String {
+    var str = "["
+    for (i in list) {
+        str += i.JSONer() + ","
+    }
+    str = str.dropLast(1)
+    str += "]"
     return str
 }
