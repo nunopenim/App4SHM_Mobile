e6d3ca62a7cdfaedc4fade5a6cd6661e0a1a25d2
nunopenim <nuno.penim@gmail.com>
2021-04-14 11:31:01 +0100

    MainActivity: added a Toast to warn the user about AutoTime stuff

--- app/src/main/java/org/app4shm/demo/MainActivity.kt
+++ app/src/main/java/org/app4shm/demo/MainActivity.kt
@@ -3,8 +3,11 @@ package org.app4shm.demo
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -20,8 +23,13 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT; //rotation
+        if (Settings.Global.getInt(getContentResolver(), Settings.Global.AUTO_TIME) == 0) {
+            Log.e("Time", "Auto-time is off!")
+            val toast = Toast.makeText(this, getString(R.string.no_auto_time), Toast.LENGTH_LONG)
+            toast.show()
+        }
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
